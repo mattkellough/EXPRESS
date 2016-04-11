4152a086bbc09510cf003ba828009a1958c22971
Matt Kellough <mrk10a@gmail.com>
2016-04-11 18:44:42 -0400

    added data entries to mongo

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/my_app')
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,11 +25,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(function(req, res, next){
+  req.db = db;
+  next();
+});
 app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,14 @@ router.get('/helloworld', function(req, res){
   res.render('helloworld', { title: 'Hello, World!'});
 });
 
+router.get('/userlist', function(req, res){
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e,docs){
+    res.render('userlist', {
+      "userlist" : docs
+    });
+  });
+});
+
 module.exports = router;
